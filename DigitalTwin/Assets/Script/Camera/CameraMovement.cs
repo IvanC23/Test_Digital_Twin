00ce3640982bc0d17b67e6f1c7cc25af60e84020
IvanC23 <crusco.ivan2@gmail.com>
2024-01-12 00:24:45 +0100

    Movement camera

--- DigitalTwin/Assets/Script/Camera/CameraMovement.cs
+++ DigitalTwin/Assets/Script/Camera/CameraMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 50f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Movimento
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+        // Rotazione sull'asse Y
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+
+        // Rotazione sull'asse X
+        if (Input.GetKey(KeyCode.R))
+        {
+            transform.Rotate(Vector3.right, -rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        }
     }
 }
