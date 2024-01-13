7a9a42dbf43401772855af46fe1cc7d42d333824
IvanC23 <crusco.ivan2@gmail.com>
2024-01-13 23:51:55 +0100

    Making camera move also during timeScale = 0

--- DigitalTwin/Assets/Script/Camera/CameraMovement.cs
+++ DigitalTwin/Assets/Script/Camera/CameraMovement.cs
@@ -6,37 +6,53 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [Header("Parametri configurabili")]
-    [SerializeField] private float _moveSpeed = 15f;
+    [SerializeField] private float _moveSpeed = 8f;
     [SerializeField] private float _rotationSpeed = 200f;
     private bool _isOnTopView = false;
     private bool _isTPressed = false;
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
         //GROUNDED CAMERA BRANCH
         if (!_isOnTopView)
         {
-            UnityEngine.Vector3 movement = new UnityEngine.Vector3(horizontal, 0f, vertical) * _moveSpeed * Time.deltaTime;
-            transform.Translate(movement);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(-1f, 0f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            else
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(1f, 0f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 0f, 1f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 0f, -1f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
             if (Input.GetKey(KeyCode.R))
             {
-                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 1f, 0f) * _moveSpeed * Time.deltaTime;
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 1f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
                 transform.Translate(upMovement);
             }
             else if (Input.GetKey(KeyCode.F))
             {
-                UnityEngine.Vector3 downMovement = new UnityEngine.Vector3(0f, -1f, 0f) * _moveSpeed * Time.deltaTime;
+                UnityEngine.Vector3 downMovement = new UnityEngine.Vector3(0f, -1f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
                 transform.Translate(downMovement);
             }
             if (Input.GetKey(KeyCode.Q))
             {
-                transform.Rotate(UnityEngine.Vector3.up, -_rotationSpeed * Time.deltaTime);
+                transform.Rotate(UnityEngine.Vector3.up, -_rotationSpeed * Time.unscaledDeltaTime);
             }
             else if (Input.GetKey(KeyCode.E))
             {
-                transform.Rotate(UnityEngine.Vector3.up, _rotationSpeed * Time.deltaTime);
+                transform.Rotate(UnityEngine.Vector3.up, _rotationSpeed * Time.unscaledDeltaTime);
             }
             if (Input.GetKey(KeyCode.T))
             {
@@ -57,25 +73,44 @@ public class CameraMovement : MonoBehaviour
         //FLYING CAMERA BRANCH
         else
         {
-            UnityEngine.Vector3 movement = new UnityEngine.Vector3(horizontal, vertical, 0f) * _moveSpeed * Time.deltaTime;
-            transform.Translate(movement);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(-1f, 0f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            else
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(1f, 0f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 1f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
+            else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, -1f, 0f) * _moveSpeed * Time.unscaledDeltaTime;
+                transform.Translate(upMovement);
+            }
             if (Input.GetKey(KeyCode.R))
             {
-                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 0f, -1f) * _moveSpeed * Time.deltaTime;
+                UnityEngine.Vector3 upMovement = new UnityEngine.Vector3(0f, 0f, -1f) * _moveSpeed * Time.unscaledDeltaTime;
                 transform.Translate(upMovement);
             }
             else if (Input.GetKey(KeyCode.F))
             {
-                UnityEngine.Vector3 downMovement = new UnityEngine.Vector3(0f, 0f, 1f) * _moveSpeed * Time.deltaTime;
+                UnityEngine.Vector3 downMovement = new UnityEngine.Vector3(0f, 0f, 1f) * _moveSpeed * Time.unscaledDeltaTime;
                 transform.Translate(downMovement);
             }
             if (Input.GetKey(KeyCode.Q))
             {
-                transform.Rotate(UnityEngine.Vector3.forward, _rotationSpeed * Time.deltaTime);
+                transform.Rotate(UnityEngine.Vector3.forward, _rotationSpeed * Time.unscaledDeltaTime);
             }
             else if (Input.GetKey(KeyCode.E))
             {
-                transform.Rotate(UnityEngine.Vector3.forward, -_rotationSpeed * Time.deltaTime);
+                transform.Rotate(UnityEngine.Vector3.forward, -_rotationSpeed * Time.unscaledDeltaTime);
             }
             if (Input.GetKey(KeyCode.T))
             {
